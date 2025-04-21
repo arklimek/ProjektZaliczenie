9a30d494d3b1c9d4be6f27aa7fd9f45d5cc1b5d5
arklimek <arklimek@student.agh.edu.pl>
2025-04-21 21:38:13 +0200

    Demo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import simplepbr
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import PointLight  # Import klasy PointLight
 
 
 class Demo(ShowBase):
@@ -7,20 +7,14 @@ class Demo(ShowBase):
         ShowBase.__init__(self)
         self.loadModels()
 
+        simplepbr.init()
+
     def loadModels(self):
         self.characters = loader.loadModel("Assets/Characters/characters.gltf")
         self.characters.reparentTo(render)
 
         self.enviroment = loader.loadModel("Assets/Enviroment/scene.gltf")
         self.enviroment.reparentTo(render)
-
-        point_light = PointLight("point_light")
-        point_light.setColor((1000, 1000, 1000, 1))  # Intensywność i kolor (biały)
-        point_light.setAttenuation((0, 0, 1))  # Tłumienie kwadratowe dla PBR
-
-        # Umieszczenie światła w scenie
-        light_np = render.attachNewNode(point_light)
-        light_np.setPos(0, 0, 0)  # Pozycja (0, 0, 0)
 
 
 test = Demo()
