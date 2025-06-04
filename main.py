a8a9bd704951a485bab394ab919e8fc3c4be274a
Arkadiusz Klimek <arklimek@student.agh.edu.pl>
2025-06-04 20:02:18 +0200

    Demo

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
-
+import sys
 import simplepbr
 from direct.gui.DirectGui import *
 from direct.showbase.ShowBase import ShowBase
 from pandac.PandaModules import *
+from panda3d.core import PointLight, VBase4
 
-#ConfigVariableBool("fullscreen", 0).setValue(1)
+ConfigVariableBool("fullscreen", 0).setValue(1)
 
 load_prc_file("myConfig.prc")
 
 index_character_folder = 1
 index_character = 1
 index_enviroment = 1
+rotate_character = 0
 
 class Demo(ShowBase):
     def checkfile(directory, filename):
@@ -52,17 +54,26 @@ class Demo(ShowBase):
                 del self.loadingScreenNode
                 del self.loadingText
 
+    def cameralocation(self):
+        self.disableMouse()
+
+        self.camLens.set_fov(65)
+        self.cam.set_hpr(0, -10, 0)
+        self.cam.set_pos(-0.5, -1.5, 1.75)
+
+        self.lastMouseX = 0
+        self.accept("mouse1", self.startMouseTask)
+        self.accept("mouse1-up", self.stopMouseTask)
+
     def __init__(self):
         ShowBase.__init__(self)
         self.loadingscreen(True)
-
         self.loadModels()
         self.createlight()
-        self.cameracontrol()
+        self.cameralocation()
         self.setskybox()
         self.setfog()
         self.createGUI()
-
         self.loadingscreen(False)
 
         simplepbr.init(
@@ -72,10 +83,38 @@ class Demo(ShowBase):
             max_lights=2
         )
 
-    def cameracontrol(self):
-        self.camLens.set_fov(90)
-        self.cam.set_hpr(0, 0, 0)
-        self.cam.set_pos(0., -2., 2)
+        self.myMusic = self.loader.loadSfx('Assets/Audio/1.mp3')
+        self.ambient = self.loader.loadSfx('Assets/Audio/ambient.mp3')
+        self.ambient.play()
+
+    def startMouseTask(self):
+        self.taskMgr.add(self.mouseTask, "mouseTask")
+
+    def stopMouseTask(self):
+        self.taskMgr.remove("mouseTask")
+
+    def mouseTask(self, task):
+        if self.mouseWatcherNode.hasMouse():
+            mpos = self.mouseWatcherNode.getMouse()
+            mouseX = mpos.getX()
+
+            deltaX = mouseX - self.lastMouseX
+
+            currentH = self.camera.getH()
+            self.camera.setH(currentH - deltaX * 80)
+
+            self.lastMouseX = mouseX
+
+        return task.cont
+
+    def characterrotate(self):
+        global rotate_character
+        self.characters.setHpr(rotate_character, 0, 0)
+
+    def slider(self):
+        global rotate_character
+        rotate_character = self.slider['value']
+        self.characterrotate()
 
     def loadModels(self):
         global index_character, index_enviroment, index_character_folder
@@ -83,11 +122,12 @@ class Demo(ShowBase):
         self.characters = loader.loadModel(f"Assets/Characters/{index_character_folder}/{index_character}.gltf")
         self.characters.reparentTo(render)
         self.characters.setScale(1, 1, 1)
-        self.characters.setPos(-2, 3, 0)
-        self.characters.setHpr(475, 0, 0)
+        self.characters.setPos(0, 2, 0)
+        self.characters.setHpr(0, 0, 0)
 
         self.enviroment = loader.loadModel(f"Assets/Enviroment/{index_enviroment}/scene.gltf")
         self.enviroment.reparentTo(render)
+
 
     def createlight(self):
         self.ambient = AmbientLight('ambient')
@@ -122,7 +162,7 @@ class Demo(ShowBase):
         self.skybox.setLightOff(0)
 
     def swapcharacter(self, direction=1):
-        global index_character
+        global index_character, rotate_character
 
         max_models = 50
         folder = index_character_folder
@@ -147,8 +187,8 @@ class Demo(ShowBase):
         self.characters = loader.loadModel(path)
         self.characters.reparentTo(render)
         self.characters.setScale(1, 1, 1)
-        self.characters.setPos(-2, 3, 0)
-        self.characters.setHpr(475, 0, 0)
+        self.characters.setPos(0, 2, 0)
+        self.characters.setHpr(rotate_character, 0, 0)
 
     def swapenviroment(self, direction=1):
         global index_enviroment
@@ -176,7 +216,7 @@ class Demo(ShowBase):
         self.enviroment.reparentTo(render)
 
     def swapcharacterfolder(self, direction=1):
-        global index_character_folder, index_character
+        global index_character_folder, index_character, rotate_character
 
         max_folders = 50
 
@@ -202,81 +242,149 @@ class Demo(ShowBase):
         self.characters = loader.loadModel(path)
         self.characters.reparentTo(render)
         self.characters.setScale(1, 1, 1)
-        self.characters.setPos(-2, 3, 0)
-        self.characters.setHpr(475, 0, 0)
+        self.characters.setPos(0, 2, 0)
+        self.characters.setHpr(rotate_character, 0, 0)
 
+    def audioplay(self):
+        if self.myMusic.status() == AudioSound.PLAYING:
+            self.myMusic.stop()
+        else:
+            self.myMusic.setVolume(0.5)
+            self.myMusic.play()
 
     def createGUI(self):
 
-        image = OnscreenImage(
+        '''image = OnscreenImage(
             parent=self.a2dBottomCenter,
             image='Assets/Hud/3.png',
             pos=(0, 0, 0.4),
             scale=(1, 1, 0.4),
             hpr=(0, 0, 0)
-        )
+        )'''
 
-        image.setTransparency(TransparencyAttrib.MAlpha)
+        self.text_character = OnscreenText(text="CHARACTER", pos=(0.45, -0.15), scale=0, fg=(1, 0, 0, 1),
+                                           align=TextNode.ACenter, parent=self.a2dTopLeft)
 
-        self.typ_postaci_prev = DirectButton(
+        self.text_preset = OnscreenText(text="PRESET", pos=(0.45, -0.5), fg=(1, 1, 1, 1), align=TextNode.ACenter,
+                                        parent=self.a2dTopLeft)
+        self.poprzedni_typ_postaci = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text=("<< TYP POSTACI"),
-            text0_font=loader.loadFont('Assets/Hud/Roboto.ttf'),
-            scale=0.05,
+            text="TP",
+            scale=0.03,
             command=lambda: self.swapcharacterfolder(-1),
-            pos=(-0.5, 0, -0.9)
+            pos=(0.25, 0, -0.48),
+            image="Assets/Hud/left.png",
+            parent=self.a2dTopLeft
         )
 
-        self.typ_postaci_next = DirectButton(
+        self.kolejny_typ_postaci = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text=">> TYP POSTACI",
-            scale=0.05,
+            text="TP",
+            scale=0.03,
             command=lambda: self.swapcharacterfolder(1),
-            pos=(0.5, 0, -0.9)
+            pos=(0.65, 0, -0.48),
+            image="Assets/Hud/right.png",
+            parent=self.a2dTopLeft
         )
+
+        self.text_skin = OnscreenText(text="SKIN", pos=(0.45, -1), fg=(1, 1, 1, 1), align=TextNode.ACenter,
+                                      parent=self.a2dTopLeft)
         self.poprzednia_postac = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text="<< POSTAĆ",
-            scale=0.05,
+            text="p",
+            scale=0.03,
             command=lambda: self.swapcharacter(-1),
-            pos=(-0.3, 0, -0.9)
+            pos=(0.25, 0, -0.98),
+            image="Assets/Hud/left.png",
+            parent=self.a2dTopLeft
         )
 
         self.kolejna_postac = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text=">> POSTAĆ",
-            scale=0.05,
+            text="p",
+            scale=0.03,
             command=lambda: self.swapcharacter(1),
-            pos=(0.3, 0, -0.9)
+            pos=(0.65, 0, -0.98),
+            image="Assets/Hud/right.png",
+            parent=self.a2dTopLeft
         )
 
+        self.text_background = OnscreenText(text="BACKGROUND", pos=(-0.45, -0.15), scale=0, fg=(0, 1, 0, 1),
+                                            align=TextNode.ACenter, parent=self.a2dTopRight)
         self.poprzednia_mapa = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text="<< MAPA",
-            scale=0.05,
+            scale=0.03,
             command=lambda: self.swapenviroment(-1),
-            pos=(-0.1, 0, -0.8)
+            pos=(-0.72, 0, -0.13),
+            image="Assets/Hud/left.png",
+            parent=self.a2dTopRight
         )
 
         self.kolejna_mapa = DirectButton(
             text_bg=(0, 0, 0, 0),
             text_fg=(1, 1, 1, 1),
             frameColor=(0, 0, 0, 0),
-            text=">> MAPA",
-            scale=0.05,
+            scale=0.03,
             command=lambda: self.swapenviroment(1),
-            pos=(0.1, 0, -0.8)
+            pos=(-0.18, 0, -0.13),
+            image="Assets/Hud/right.png",
+            parent=self.a2dTopRight
         )
+
+        self.exit = DirectButton(
+            text_bg=(0, 0, 0, 0),
+            text_fg=(1, 1, 1, 1),
+            frameColor=(0, 0, 0, 0),
+            text=("EXIT"),
+            scale=0.05,
+            command=lambda: sys.exit(),
+            pos=(-0.4, 0, 0.1),
+            parent=self.a2dBottomRight
+        )
+        self.audio = DirectButton(
+            text_bg=(0, 0, 0, 0),
+            text_fg=(1, 1, 1, 1),
+            frameColor=(0, 0, 0, 0),
+            scale=0.035,
+            command=self.audioplay,
+            pos=(0.1, 0, 0.1),
+            image="Assets/Audio/play.png",
+            parent=self.a2dBottomLeft
+        )
+        self.slider = DirectSlider(
+            range=(0, 720),
+            value=rotate_character,
+            pageSize=1,
+            command=self.slider,
+            pos=(0, 0, 0.1),
+            scale=0.3,
+            parent=self.a2dBottomCenter,
+            thumb_frameColor=(1, 1, 1, 0.25),
+            frameColor=(1, 1, 1, 0.15),
+            text_fg=(0, 1, 0, 1)
+        )
+
+        myFrame = DirectFrame(frameColor=(0, 0, 0, 0.2), frameSize=(-10, -0.5, -10, 10), pos=(1.4, 0, 0),
+                              parent=self.a2dLeftCenter)
+
+        # self.image.setTransparency(TransparencyAttrib.MAlpha)
+        self.poprzednia_mapa.setTransparency(TransparencyAttrib.MAlpha)
+        self.kolejna_mapa.setTransparency(TransparencyAttrib.MAlpha)
+        self.poprzednia_postac.setTransparency(TransparencyAttrib.MAlpha)
+        self.kolejna_postac.setTransparency(TransparencyAttrib.MAlpha)
+        self.poprzedni_typ_postaci.setTransparency(TransparencyAttrib.MAlpha)
+        self.kolejny_typ_postaci.setTransparency(TransparencyAttrib.MAlpha)
+        self.audio.setTransparency(TransparencyAttrib.MAlpha)
 
 
 test = Demo()
--- myConfig.prc
+++ myConfig.prc
@@ -1,7 +1,8 @@
 show-frame-rate-meter #f
 sync-video #t
 fullscreen #f
-text-default-font
+text-default-font Assets/Hud/SpaceMono.ttf
+text-encoding utf8
 
 #ifdef _WIN32
 extern "C" {
